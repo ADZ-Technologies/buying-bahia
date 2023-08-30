51cd5ba24837e3ccb64c107f260def5982208175
Brad S <brad@adztech.io>
2023-08-29 18:31:46 -0600

    init

--- app/components/FeaturedBlog.jsx
+++ app/components/FeaturedBlog.jsx
@@ -12,17 +12,17 @@ export default async function FeaturedBlog() {
     <section className="flex flex-row">
         <div className="relative h-screen w-1/2 group overflow-hidden">
           <Image  
-            src={feature.image} 
+            src={feature?.image} 
             fill={true}
             className="brightness-50 group-hover:brightness-100 group-hover:scale-110 transition-all duration-1000 object-cover"
             alt="Featured Blog Image"
           />
                   <div className="flex flex-col space-y-4 absolute p-4 transition-colors duration-1000 group-hover:bg-neutral-900/50 shadow-sm rounded-br-sm">
                             <span className="group-hover:brightness-125 text-4xl group-hover:font-medium font-brand text-neutral-100">
-                                {feature.title}
+                                {feature?.title}
                             </span>
                             <span className="text-neutral-100 text-lg">
-                              {feature.caption}
+                              {feature?.caption}
                             </span>
                             </div>
                                   <button className="invisible group-hover:visible absolute bottom-4 right-4 button bg-sky-800">
@@ -30,24 +30,24 @@ export default async function FeaturedBlog() {
                         </button>
         </div>
         <div className="w-1/2 grid grid-cols-2">
-          {grid.map((post) => {
+          {grid?.map((post) => {
             return (
               <div 
-                key={post.id} 
+                key={post?.id} 
                 className="relative h-full w-full group overflow-hidden"
               >
               <Image  
-              src={post.image} 
+              src={post?.image} 
               fill={true}
               className="brightness-50 group-hover:brightness-100 group-hover:scale-110 transition-all duration-1000 object-cover"
               alt="Blog Image"
             />
                             <div className="flex flex-col space-y-4 absolute p-4 transition-colors duration-1000 group-hover:bg-neutral-900/50 shadow-sm rounded-br-sm">
                             <span className="group-hover:brightness-125 group-hover:font-medium text-2xl font-brand text-neutral-100">
-                                {post.title}
+                                {post?.title}
                             </span>
                             <span className="invisible group-hover:visible text-sm text-neutral-100">
-                              {post.caption}
+                              {post?.caption}
                             </span>
                             </div>
                                     <button className="invisible group-hover:visible absolute bottom-4 right-4 button bg-sky-800">
