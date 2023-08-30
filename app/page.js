706d406ee37a7d13e1a73025ed05aa5b694fd3fb
Brad S <brad@adztech.io>
2023-08-29 19:16:24 -0600

    init

--- app/page.js
+++ app/page.js
@@ -5,10 +5,10 @@ import FeaturedBlog from "./components/FeaturedBlog"
 
 const page = async () => {
 
-  const blog = await fetch(process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/blog' : 'https://buying-bahia.vercel.app/api/blog').then(res => res.json())
+  const blog = await fetch(process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/blog' : '/api/blog').then(res => res.json())
   const featured = ['34981','39068','39864','39454']
 
-  const properties = await fetch(process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/featured' : 'https://buying-bahia.vercel.app/api/featured', {  
+  const properties = await fetch(process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/featured' : '/api/featured', {  
       method: 'POST',
       body: JSON.stringify(featured)
   }).then((res) => res.json())
